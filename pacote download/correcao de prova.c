437e347e240b18ddeda6be87a148d304271c4f0a
Isabela-Linhares-Silva <isabelalinharessilva@gmail.com>
2024-05-27 11:35:59 -0300

    new

---
+++ pacote download/correcao de prova.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int i,j;
+    float nota[3]={0},media,soma;
+    char gaba[5],resp[5],aluno[3][100];
+    printf("----------------------------------\n");
+    printf("   PASSO 1- Cadastro Gabarito\n");
+    printf("----------------------------------\n");
+    for(i=1;i<6;i++)
+    {
+        printf("Questao %d: ",i);
+        scanf("%c",&gaba[i]);
+        fflush(stdin);
+    }
+    for(j=1;j<4;j++)
+    {
+        printf("-----------------\n");
+        printf("     ALUNO %d\n",j);
+        printf("-----------------\n");
+        printf("Nome: ");
+        scanf("%s",&aluno[j]);
+        printf("RESPOSTAS DADAS\n");
+        for(i=1;i<6;i++)
+        {
+            fflush(stdin);
+            printf("Questao %d: ",i);
+            scanf("%c",&resp[i]);
+        }
+
+    }
+    for(i=1;i<6;i++)
+        {
+            if (resp[i] == gaba[i])
+            {
+                nota[j]+=1;
+            }
+        }
+        printf("nota do aluno %s: %.2f\n",aluno[j],nota[j]);
+    soma += nota[j];
+    media = soma/3;
+    printf("A media da tuma foi: %.2f",media);
+    return 0;
+}
