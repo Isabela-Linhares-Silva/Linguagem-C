cf17d366a7e2c1080998998f150366fedbff6d90
Isabela-Linhares-Silva <isabelalinharessilva@gmail.com>
2024-10-01 09:16:00 -0300

    struct

---
+++ pacote download/Estudos soltos/struct 1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    struct ficha_aluno
+    {
+        char nome[50];
+        int matricula;
+        float nota;
+    };// definição da struct
+    struct ficha_aluno aluno;
+    printf("CADASTRO\n");
+    printf("Nome do aluno: ");
+    gets(aluno.nome);
+    printf("Matricula: ");
+    scanf("%d",&aluno.matricula);
+    printf("Nota: ");
+    scanf("%f",&aluno.nota);
+    printf("\n\nMOSTRA\n\n");
+    printf("Nome: %s\n",aluno.nome);
+    printf("Nota:%.2f",aluno.nota);
+
+    return 0;
+}
